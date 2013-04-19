3bcaa0f235d6fd2292da7219026db161fa3f380e
Daniel Reyes <Dreyes007@fullsail.edu>
2013-04-18 22:42:39 -0400

    added JQuery to js file

--- js/main.js
+++ js/main.js
@@ -2,36 +2,52 @@
 // ASDI 1304
 // javaScript for GPeS app
 
-
-var localStorageKey = "storeKey";
-$('#information').live('pagecreate', function() {
-            if (Modernizr.localstorage) {
-                alert("Information Saved!")
-            }
-            else {
-                $('#message').text("Unfortunately your browser doesn't support local storage");
-                $('#addToStorage').attr('disabled', 'disabled');
-                $('#message').show();
-            }
-            $('#submit').click(function(e) {
-                //localStorage.setItem(localStorageKey, $('#fname').val());
-                $("localStorage").data("First Name", "#fname")
-
-                showStoreValue();
-                e.preventDefault();
-            });
-        });
-
-        function showStoreValue() {
-            var item = localStorage.getItem(localStorageKey);
-            if (item == null) {
-                item = 'Nothing in store';
-            }
-            else if (item.length === 0) {
-                item = 'Store contains empty value';
-            }
-            $('.storeItem').text(item);
-        }
+$('#home').on('pageinit', function (){
 
 	
 	
+	
+});
+// Store form values to Local Storage.
+	var id			= Math.floor(Math.random()*100000001);
+$('#surveyInfo').on('pageinit', function(){
+	if(modernizr.localStorage){
+		$('#message').text("Form is ready");
+	}else{
+		$('#message').text("Local Storage Not Supported");
+	}
+	$('#submit').click(function(){
+		localStorage.setItem(id, $('#fname').val());
+		localStorage.setItem(id, $('#lname').val());
+		localStorage.setItem(id, $('#gValue').val());
+		localStorage.setItem(id, $('#date').val());
+		localStorage.setItem(id, $('#rating').val());
+		localStorage.setItem(id, $('#callback').val());
+	});
+	
+	function getRadio(){
+		var radios = document.forms[0].gender;
+		for(var i=0; i<radios.length; i++){
+			if(radios[i].checked){
+				gValue = radios[i].value;
+			}
+		}	
+	}	
+});
+// Show store values from Local Storage
+$('#survey').on('pageinit', function (){
+	
+	function showStoreValue(){
+		var item = localStorage.getItem(id);
+		if (item == null){
+			item = 'Nothing in Storage';
+		}else if(item.length === 0){
+			item = 'Storage Contains Empty Value';
+		}
+		$('.storage').text(item)
+	}
+$('#survey').click(function(e){
+	showStoreValue();
+});
+	
+});
